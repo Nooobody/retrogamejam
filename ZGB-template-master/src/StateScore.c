4ed11031c05cf1051b8db135c203af2a1c189112
JuhoK-R <juho.keski-rahkonen@maplet.com>
2019-10-05 20:53:38 +0300

    Fix positioning

--- ZGB-template-master/src/StateScore.c
+++ ZGB-template-master/src/StateScore.c
@@ -24,35 +24,35 @@ void Start_StateScore()
 
         switch (digit) {
           case 1:
-            SpriteManagerAdd(SpriteOne, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteOne, 112 - 8 * i, 56);
             break;
           case 2:
-            SpriteManagerAdd(SpriteTwo, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteTwo, 112 - 8 * i, 56);
             break;
           case 3:
-            SpriteManagerAdd(SpriteThree, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteThree, 112 - 8 * i, 56);
             break;
           case 4:
-            SpriteManagerAdd(SpriteFour, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteFour, 112 - 8 * i, 56);
             break;
           case 5:
-            SpriteManagerAdd(SpriteFive, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteFive, 112 - 8 * i, 56);
             break;
           case 6:
-            SpriteManagerAdd(SpriteSix, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteSix, 112 - 8 * i, 56);
             break;
           case 7:
-            SpriteManagerAdd(SpriteSeven, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteSeven, 112 - 8 * i, 56);
             break;
           case 8:
-            SpriteManagerAdd(SpriteEight, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteEight, 112 - 8 * i, 56);
             break;
           case 9:
-            SpriteManagerAdd(SpriteNine, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteNine, 112 - 8 * i, 56);
             break;
           case 0:
           default:
-            SpriteManagerAdd(SpriteZero, 104 - 8 * i, 56);
+            SpriteManagerAdd(SpriteZero, 112 - 8 * i, 56);
             break;
         }
     }
